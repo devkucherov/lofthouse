113652a157ce68ac511642c9626561709e691782
Pasha Kucherov <dev.kucherov@gmail.com>
2023-07-12 20:13:33 +0300

    добавить скрипт маски и яндекс карт

--- js/main.js
+++ js/main.js
@@ -8,3 +8,36 @@ navBtn.onclick = function () {
   document.body.classList.toggle('no-scroll');
 
 }
+
+/* Phone mask */
+mask('[data-tel-input]');
+
+// удалить '+' если больше ничего не введен, что бы показать placeholder
+const phoneInputs = document.querySelectorAll('[data-tel-input]');
+phoneInputs.forEach((input) => {
+  input.addEventListener('input', () => {
+    if (input.value == '+') input.value = '';
+  })
+  input.addEventListener('blur', () => {
+    if (input.value == '+') input.value = '';
+  })
+});
+
+/* Yandex map */
+
+// Функция ymaps.ready() будет вызвана, когда
+// загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+ymaps.ready(init);
+function init() {
+  // Создание карты.
+  var myMap = new ymaps.Map("map", {
+    // Координаты центра карты.
+    // Порядок по умолчанию: «широта, долгота».
+    // Чтобы не определять координаты центра карты вручную,
+    // воспользуйтесь инструментом Определение координат.
+    center: [55.76, 37.64],
+    // Уровень масштабирования. Допустимые значения:
+    // от 0 (весь мир) до 19.
+    zoom: 7
+  });
+}
